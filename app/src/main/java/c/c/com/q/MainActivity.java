302b41c6edde8050b5fd0c0248bdbee736bea7e4
467437736 <467437736>
2016-01-16 21:38:04 +0800

    Initial commit

---
+++ app/src/main/java/c/c/com/q/MainActivity.java
@@ -0,0 +1,27 @@
+package c.c.com.q;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+   private FragmentActivity mTabhost;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+    }
+
+
+}
